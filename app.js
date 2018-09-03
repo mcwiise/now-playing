e4ae4a4c39186e40f744fda2d765ba58f334fb3c
Germán Valencia Vargas <valennciag@gmail.com>
2018-09-03 17:22:31 -0500

    Adding Bulma support and templating the index

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const router = require("./router");
 
 router(app);
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 const server = app.listen(3000, function () {
     console.log("Welcome to now playing!: ", server.address().port);
